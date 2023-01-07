c104ad0592dcb4ac33d039dd647382c307268ca6
Akshat2403 <95174507+Akshat2403@users.noreply.github.com>
2023-01-07 21:34:43 +0530

    fixed verification

--- server/utils/verfication.js
+++ server/utils/verfication.js
@@ -1,7 +1,7 @@
 import * as jwt from 'jsonwebtoken';
 import createError from './error.js';
 
-export const verifyToken = (req, res, next, temp) => {
+export const verifyToken = (req, res, next, userCheck) => {
     const token = req.cookies.access_token;
     if (!token) {
         return next(createError(401, 'You are not Authenticated'));
@@ -11,13 +11,12 @@ export const verifyToken = (req, res, next, temp) => {
             return next(createError(403, 'Token is not valid'));
         }
         req.user = user;
-        temp();
+        userCheck();
         next();
     });
 };
 export const verifyuser = (req, res, next) => {
     verifyToken(req, res, next, () => {
-        console.log(req.params.uid);
         if (req.user.id != req.params.uid) {
             return next(createError(403, 'You are not authorized'));
         }
