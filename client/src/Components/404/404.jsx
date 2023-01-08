5ea663d598b84b3f2ad4eadbf407d6a42c6ca4b5
Geetmanik <geetmanik2@gmail.com>
2023-01-08 21:17:30 +0530

    Error-page responsive

--- client/src/Components/404/404.jsx
+++ client/src/Components/404/404.jsx
@@ -1,24 +1,22 @@
 import './404.css';
-import error from './error.png';
-import emoji from './emoji.png';
+
 import logo from './logo1.png';
-import Background from './Background-search.png';
+
 import NotFound from './not found.png';
 
 const Error = () => {
     return (
         <>
+            <div className="Logo-top">
+                <div>
+                    <img src={logo} alt="" />
+                </div>
+                <div className="Heading-top">Echo</div>
+            </div>
             <div className="Error-main">
-                <div className="Logo-top">
-                    <div>
-                        <img src={logo} alt="" />
-                    </div>
-                    <div className="Heading-top">Echo</div>
-                </div>
                 <div className="Error-404">
-                    <img className="Error-Not-Found" src={NotFound}></img>
+                    <img src={NotFound}></img>
                 </div>
-                <img className="Error-name" src={error}></img>
             </div>
         </>
     );
